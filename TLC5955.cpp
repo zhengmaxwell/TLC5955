0ad0f2338dd16d9fbd77d40f1b3a71926e0c354d
Maxwell Zheng <maxwell@ramonaoptics.com>
2021-10-26 17:33:04 -0400

    fix getGrayscale typo

--- TLC5955.cpp
+++ TLC5955.cpp
@@ -341,7 +341,7 @@ void TLC5955::setLed(uint16_t led_number, uint16_t rgb)
 }
 
 // Assume all LEDs are the same
-uint8_t* TLC5955:getGrayscale()
+uint8_t* TLC5955::getGrayscale()
 {
   uint8_t grayscale[] = {_grayscale_data[0][0][0], _grayscale_data[0][0][1], _grayscale_data[0][0][2]};
   return grayscale;
