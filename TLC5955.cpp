d56bf71a9abd1dd278a83dde0df6754455c7696f
Maxwell Zheng <maxwell@ramonaoptics.com>
2021-10-29 12:42:36 -0400

    dev test

--- TLC5955.cpp
+++ TLC5955.cpp
@@ -347,7 +347,7 @@ void TLC5955::getLedCurrents(float currents[])
   for (int i = 0; i < 3; i++)
   {
     currents[i] = maxCurrentValues[_MC[i]] * (0.262 + 0.738 * _DC[i] / 127)
-              * (0.1 + 0.9 * _BC[i] / 127)); //* _grayscale_data[0][0][i] / 65535);
+              * (0.1 + 0.9 * _BC[i] / 127); //* _grayscale_data[0][0][i] / 65535);
   }
 }
 
