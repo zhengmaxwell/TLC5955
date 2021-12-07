d944eb2cdcf4a8b55553a4b288b8ee3a5af2efd0
Maxwell Zheng <maxwell@ramonaoptics.com>
2021-12-07 16:52:39 -0500

    refactor code

--- TLC5955.cpp
+++ TLC5955.cpp
@@ -193,23 +193,14 @@ void TLC5955::setControlModeBit(bool is_control_mode)
 
 int TLC5955::updateLeds()
 {
-  // Get number of counts for current pattern
-  uint32_t power_output_counts = 0;
-  for (int16_t chip = (int8_t)_tlc_count - 1; chip >= 0; chip--)
-    for (int8_t led_channel_index = (int8_t)LEDS_PER_CHIP - 1; led_channel_index >= 0; led_channel_index--)
-      for (int8_t color_channel_index = (int8_t)COLOR_CHANNEL_COUNT - 1; color_channel_index >= 0; color_channel_index--)
-        power_output_counts += _grayscale_data[chip][led_channel_index][color_channel_index];
-  if (power_output_counts == 0) {
+  double power_output_amps = getTotalCurrent();
+  if (enforce_max_current && power_output_amps > max_current_amps)
+    return 1;
+
+  if (power_output_amps == 0) {
     analogWrite(_gsclk, 0);
   } else {
     analogWrite(_gsclk, 1);
-  }
-
-  if (enforce_max_current)
-  {
-    double power_output_amps = getTotalCurrent();
-    if (power_output_amps > max_current_amps)
-      return 1;
   }
 
   for (int16_t chip = (int8_t)_tlc_count - 1; chip >= 0; chip--)
